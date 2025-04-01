907f5c5bbea95839051cca1f6d26dc7c1e95e611
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-04-01 15:13:17 -0300

    configuring server port

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express, {Request, Response} from "express";
 
-const app = express()
+const app = express();
+const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
     res.send("hello world");
 });
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Server running at port http://localhost:${port}`);
+});
