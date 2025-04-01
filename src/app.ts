18e059321a3574353bc50e7a026b0135bb282b36
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-04-01 15:39:44 -0300

    routes created

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express, { json, Request, Response } from "express";
+import express, { json } from "express";
 import { getPlayer } from "./controllers/players-controller";
+import router from "./routes";
 
 function createApp(){
 
     const app = express();
 
     app.use(json());
-
-    app.get("/", getPlayer);
+    app.use("/api", router);
 
 return app;
 
